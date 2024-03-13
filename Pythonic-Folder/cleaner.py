7917c292f9842fe5584fefd7365f33aa5e689d04
Apkaless <106030623+Apkaless@users.noreply.github.com>
2024-03-13 20:05:23 +0300

    Add files via upload

---
+++ Pythonic-Folder/cleaner.py
@@ -0,0 +1,152 @@
+import os
+import subprocess
+from colorama import Fore, init
+
+
+
+def cleaner(user):
+
+    x = 3
+
+    deleted_files = []
+
+    temp = False
+    pre = False
+    temp2 = False
+
+    while x <=3:
+        
+        if x == 1:
+            temp = False
+            pre = False
+            temp2 = True
+            x = x - 1
+            os.chdir(f'c:/users/{user}/appdata/Local/')
+            print(f'\n{yellow}c:/users/{user}/appdata/Local/Temp\n______________________\n')
+            state = subprocess.check_output('rmdir /s /Q Temp', shell=True, stderr=subprocess.STDOUT).decode()
+            if 'Access is denied.' in state:
+                print(f'\n{green}[+] Cleaner is Finished')
+                break
+            print(f'\n{green}[+] Cleaner is Finished')
+            break
+        elif x == 2:
+            temp = False
+            pre = True
+            temp2 = False
+            x = x - 1
+            os.chdir('c:/Windows/Prefetch')
+            print(f'{yellow}c:/Windows/Prefetch\n______________________\n')
+
+        elif x == 3:
+            temp = True
+            pre = False
+            temp2 = False
+            x = x - 1
+            os.chdir('c:/Windows/Temp')
+            print(f'{yellow}c:/Windows/Temp\n______________________\n')
+            
+        elif x <= 0:
+            exit(0)
+
+        files_list = os.listdir()
+        
+
+        for file in files_list:
+
+            not_empty_folders = []
+
+            try:
+
+                state = subprocess.check_output(f'rmdir {file} /s /Q ', shell=True, stderr=subprocess.STDOUT).decode()
+
+
+                print(f'{blue}Folder Deleted --> {red}{file}\n')
+
+                deleted_files.append(file)
+
+            except subprocess.CalledProcessError as e:
+
+                    if '267' in str(e):
+
+                        state = subprocess.check_output(f'del {file}', shell=True, stderr=subprocess.STDOUT).decode()
+
+                        print(f'{green}File Deleted --> {red}{file}\n')
+
+                        deleted_files.append(file)
+
+                    elif '145' in str(e):
+
+                        try:
+
+                            os.chdir(file)
+
+                            not_empty_folders.append(file)
+
+                            for file in os.listdir():
+
+                                if os.path.isdir(file):
+
+                                    state = subprocess.check_output(f'rmdir {file} /s /Q ', shell=True, stderr=subprocess.STDOUT).decode()
+
+                                    print(f'{blue}Folder Deleted --> {red}{file}\n')
+
+                                    deleted_files.append(file)
+
+                                else:    
+                                    state = subprocess.check_output(f'del {file}', shell=True, stderr=subprocess.STDOUT).decode()
+
+                                    print(f'{green}File Deleted --> {red}{file}\n')
+
+                                    deleted_files.append(file)
+                        
+                            if temp:
+                                os.chdir('c:/Windows/Temp')
+                            elif pre:
+                                os.chdir('c:/Windows/prefetch')
+                            elif temp2:
+                                os.chdir(f'c:/users/{user}/appdata/Local/')
+                                
+                            state = subprocess.check_output(f'rmdir {not_empty_folders[0]} /s /Q ', shell=True, stderr=subprocess.STDOUT).decode()
+
+                            print(f'{blue}Folder Deleted --> {red}{not_empty_folders[0]}\n')
+
+                            deleted_files.append(not_empty_folders[0])
+
+                        except:
+                            pass
+
+    return len(deleted_files)
+
+if __name__ == '__main__':
+
+    init()
+
+    red = Fore.RED
+    blue = Fore.BLUE
+    green = Fore.GREEN
+    yellow = Fore.YELLOW
+
+    current_username = os.getlogin()
+
+    print('''
+    
+ ▄▄▄·  ▄▄▄·▄ •▄  ▄▄▄· ▄▄▌  ▄▄▄ ..▄▄ · .▄▄ · 
+▐█ ▀█ ▐█ ▄██▌▄▌▪▐█ ▀█ ██•  ▀▄.▀·▐█ ▀. ▐█ ▀. 
+▄█▀▀█  ██▀·▐▀▀▄·▄█▀▀█ ██▪  ▐▀▀▪▄▄▀▀▀█▄▄▀▀▀█▄
+▐█ ▪▐▌▐█▪·•▐█.█▌▐█ ▪▐▌▐█▌▐▌▐█▄▄▌▐█▄▪▐█▐█▄▪▐█
+ ▀  ▀ .▀   ·▀  ▀ ▀  ▀ .▀▀▀  ▀▀▀  ▀▀▀▀  ▀▀▀▀ 
+
+    ''')
+    print(f'{green}Welcome {blue}%s\n{green}This Script Cleans All The Temp Files From Your Windows\nFast And Easy To Use\n'%(current_username))
+
+    input(f'{green}Press ENTER To Start The Cleaner...')
+
+    os.system('cls')
+
+    files_deleted = cleaner(current_username)
+
+    os.system('cls')
+
+    print(f'\n{green}[+] Files Deleted : {red}{files_deleted}')
+    
+    input(f'\n{green}Press Enter To Exit...')
